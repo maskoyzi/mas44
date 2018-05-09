6fc2c515969ff81b989e1f6de31235f2397d0ad7
bincred <39111532+bincred@users.noreply.github.com>
2018-05-09 11:23:41 +0700

    Update sb.py

--- sb.py
+++ sb.py
@@ -27,7 +27,7 @@ while True:
                     if msg.toType == 2:
                         may = client.getProfile().mid
                         if may in str(msg.contentMetadata) and 'MENTION' in str(msg.contentMetadata):
-                            pilih = ['yang tag sy semoga jomblo seumur hidup','ngapain tag tag woe, kangen?','ada apa ini? ko di tag?','duhh kena tag, dianya kesepian kali yah','gk usah tag, gift tikel aja']
+                            pilih = ['ciee ciee yang tag, ah jadi malu','ngapain tag tag sih, kangen?','ada apa ini? ko di tag?','duhh kena tag, dianya kesepian kali yah','gk usah tag, gift tikel aja']
                             rslt = random.choice(pilih)
                             client.sendText(msg.to, str(rslt))
                         else:
